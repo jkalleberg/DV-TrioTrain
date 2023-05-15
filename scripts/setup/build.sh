62d2b62f38e94bcc833ac111c8c143ac71c55771
Jenna Kalleberg - Lewis <jakth2@mail.missouri.edu>
2023-05-15 18:02:33 -0500

    adding reproducible build example

---
+++ scripts/setup/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# scripts/setup/build.sh
+
+# NOTE: Begin an interactive session first!
+# source scripts/start_interactive.sh
+
+echo -e "=== scripts/setup/build.sh > start $(date)"
+
+echo -e "=== scripts/setup/build.sh > end $(date)"
