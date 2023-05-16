0a80df1da9c3b3e04e6d373aec607994c5fda216
Jenna Kalleberg - Lewis <jakth2@mail.missouri.edu>
2023-05-16 12:11:08 -0500

    adding DV containers

--- scripts/setup/build.sh
+++ scripts/setup/build.sh
@@ -7,8 +7,20 @@
 echo -e "=== scripts/setup/build.sh > start $(date)"
 
 # Load cluster-specific modules
-# NOTE: You will need to change this bash script to match your own system modules available
-# Reach out to your cluster's sys admin for installation guidelines
+# NOTE: You will need to change this bash script to 
+# match your own system modules available
+# Reach out to your cluster's sys admin for 
+# installation guidelines
 source scripts/setup/modules.sh
+
+# NOTE: both are required, since can't run the 
+# GPU version used for training 
+# on a non-GPU hardware
+
+# Install GPU-specific apptainer container
+bash scripts/setup/build_containers.sh DeepVariant-GPU
+
+# Install CPU-specific apptainer container
+bash scripts/setup/build_containers.sh DeepVariant-CPU
 
 echo -e "=== scripts/setup/build.sh > end $(date)"
---
+++ scripts/setup/build_containers.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# scripts/setup/build_containers.sh
+
+echo "=== scripts/setup/build_containers.sh > start $(date)" $1
+
+echo "INFO: Creating Apptainer CACHE/ and TMP/, if needed"
+install --directory --verbose ${APPTAINER_CACHEDIR}
+install --directory --verbose ${APPTAINER_TMPDIR}
+
+# Only want to build these Apptainer Image(s) once!
+echo "INFO: Installing Container Image(s), if necessary"
+
+if [[ $1 == 'DeepVariant-CPU' ]]; then
+  version="${BIN_VERSION_DV}"
+  image_name="deepvariant_${version}"
+  docker_name="${version}"
+  command="run_deepvariant"
+elif [[ $1 == 'DeepVariant-GPU' ]]; then
+  version="${BIN_VERSION_DV}"
+  image_name="deepvariant_${version}-gpu"
+  docker_name="${version}-gpu"
+  command="run_deepvariant"
+elif [[ $1 == 'DeepTrio-CPU' ]]; then
+  version="${BIN_VERSION_DT}"
+  image_name="deepvariant_deeptrio-${version}"
+  docker_name="deeptrio-${version}"
+  command="deeptrio/run_deeptrio"
+elif [[ $1 == 'DeepTrio-GPU' ]]; then
+  version="${BIN_VERSION_DT}"
+  image_name="deepvariant_deeptrio-${version}-gpu"
+  docker_name="deeptrio-${BIN_VERSION_DT}-gpu"
+  command="deeptrio/run_deeptrio"
+else
+  echo -e "ERROR: Invalid argument [$1] provided.\nINFO: Choices: [ DeepVariant-CPU, DeepTrio-CPU, DeepVariant-GPU, DeepTrio-GPU ]\nExiting... "
+  exit 1
+fi
+
+if test -x ./${image_name}.sif; then
+  echo -e "INFO: Image [${image_name}.sif] has already been installed"
+  apptainer run -B /usr/lib/locale/:/usr/lib/locale/ ${image_name}.sif /"opt/deepvariant/bin/${command}" --version
+else
+  echo -e "INFO: Image [${image_name}.sif] needs to be installed"
+  echo "INFO: Apptainer Image will go here: ${PWD}"
+  echo "INFO: Building Apptainer Image now... "
+  apptainer pull docker://google/deepvariant:"${docker_name}"
+  echo "Done: Building Apptainer Image"
+fi
+
+echo "=== scripts/setup/build_containers.sh > end $(date)"
