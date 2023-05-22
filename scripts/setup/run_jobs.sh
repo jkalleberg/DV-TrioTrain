caffdf75e1bf44cb0b662457c69ee50582af0495
Jenna Kalleberg - Lewis <jakth2@mail.missouri.edu>
2023-05-22 17:49:04 -0500

    adding a template for running with SLURM

---
+++ scripts/setup/run_jobs.sh
@@ -0,0 +1,13 @@
+!#/bin/bash
+# template for how to run the scripts created during setup
+
+# merge the PopVCF
+sbatch -n1 -t 10:00:00 -p BioCompute --mem=10G -A biocommunity --job-name concat_PopVCF --output triotrain/variant_calling/data/GIAB/allele_freq/%j_concat_PopVCF.out --mail-user=jakth2@mail.missouri.edu --mail-type=FAIL,END,TIME_LIMIT --wrap="bash triotrain/variant_calling/data/GIAB/allele_freq/concat_PopVCFs.sh"
+
+# download the AJtrio BAMs and
+# calculate coverage
+sbatch -n1 -t 10:00:00 -p BioCompute --mem=0 --exclusive -A biocommunity --job-name AJtrio --output triotrain/variant_calling/data/GIAB/bam/%j_download_AJtrio.out --mail-user=jakth2@mail.missouri.edu --mail-type=FAIL,END,TIME_LIMIT --wrap="bash triotrain/variant_calling/data/GIAB/bam/AJtrio.run"
+
+# download the HCtrio BAMs and
+# calculate coverage
+sbatch -n1 -t 10:00:00 -p BioCompute --mem=0 --exclusive -A biocommunity --job-name HCtrio --output triotrain/variant_calling/data/GIAB/bam/%j_download_HCtrio.out --mail-user=jakth2@mail.missouri.edu --mail-type=FAIL,END,TIME_LIMIT --wrap="bash triotrain/variant_calling/data/GIAB/bam/HCtrio.run"
