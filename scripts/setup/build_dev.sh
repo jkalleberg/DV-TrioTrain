d86e914b5f85b9a962b966c28bafd00510515b93
Jenna Kalleberg - Lewis <jakth2@mail.missouri.edu>
2023-05-26 13:39:13 -0500

    adding build dev env

--- scripts/setup/build_dev.sh
+++ scripts/setup/build_dev.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# scripts/setup/build_beam.sh
+# scripts/setup/build_dev.sh
 
-echo -e "=== scripts/setup/build_beam.sh > start $(date)"
+echo -e "=== scripts/setup/build_dev.sh > start $(date)"
 
 ##--- NOTE: ----##
 ##  You must have an interactive session
 ##  with more mem than defaults to work!
 ##--------------##
 
-if [ ! -d ./miniconda_envs/beam_v2.30 ] ; then
-     # If missing an enviornment called "beam_v2.30", 
+if [ ! -d ./miniconda_envs/dev ] ; then
+     # If missing an enviornment called "dev", 
      # initalize this env with only the anaconda package 
-     conda create --yes --prefix ./miniconda_envs/beam_v2.30
+     conda create --yes --prefix ./miniconda_envs/dev
 fi
 
 # Then, activate the new environment
 source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda deactivate
-conda activate ./miniconda_envs/beam_v2.30
+conda activate ./miniconda_envs/dev
 
 ##--- Configure an environment-specific .condarc file ---##
 ## NOTE: Only performed once:
@@ -34,7 +34,7 @@ conda config --env --set channel_priority flexible
 
 # Install the project-specific packages
 # in the currently active env
-conda install -p ./miniconda_envs/beam_v2.30 -y -c conda-forge python=3.8 pandas numpy python-dotenv python-snappy tensorflow=2.5 apache-beam=2.30 regex spython natsort rtg-tools
+conda install -p ./miniconda_envs/dev -y -c conda-forge python=3.8 pandas numpy python-dotenv regex spython natsort rtg-tools mkdocs mkdocs-material black
 
 # Deactivate the conda env to continue with build process
 conda deactivate
@@ -43,9 +43,6 @@ conda deactivate
 ### Python = Apache Beam Python SDK only supports v3.6-3.8
 ### Scipy = scientific libraries for Python
 ### DotEnv = enables environment variable configuration across bash and python
-### Snappy = A fast compressor/decompressor (required)
-### Apache-Beam = unified programming model for batch and stream processes
-### Tensorflow = eval metrics visualizations via TensorBoard for CPU, use v2.5.0 for DV v1.4
 ### Regex = required for update regular expression handling
 ### Spython = interface between Singularity/Apptainer bash commands and Python
 ### Natsort = enables sorting of file iterators
