fed14300dc8fcf7b3ccf8d43d601fa4752b2d28f
Jenna Kalleberg - Lewis <jakth2@mail.missouri.edu>
2023-06-22 17:11:34 -0500

    splitting up pipeline moduels

---
+++ triotrain/model_training/pipeline/phase.py
@@ -0,0 +1,14 @@
+def process_phase(txt: str):
+    """
+    Handle any special characters and only use '_' as a separator.
+
+    Input: 'A,Quick brown-fox jumped-over-the   lazy-dog'
+    Output: 'A_Quick_brown_fox_jumped_over_the_lazy_dog'
+    """
+    special_chars = "!#$%^&*()"
+    for special_char in special_chars:
+        txt = txt.replace(special_char, "")
+    standardize_seps = " -,"
+    for sep in standardize_seps:
+        txt = txt.replace(sep, "_")
+    return txt
---
+++ triotrain/model_training/pipeline/resources.py
@@ -0,0 +1,14 @@
+def process_resource(txt: str):
+    """
+    Handle any special characters and remove any separators.
+
+    Input: 'A,Quick brown-fox jumped-over-the   lazy-dog'
+    Output: 'AQuickbrownfoxjumpedoverthelazydog'
+    """
+    specialChars = "!#$%^&*()"
+    for specialChar in specialChars:
+        txt = txt.replace(specialChar, "")
+    standardizeSeps = " -,_"
+    for sep in standardizeSeps:
+        txt = txt.replace(sep, "")
+    return txt
