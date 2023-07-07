db3275b6cd7f90a28d7014d9aff9b796ac64746d
Jenna Kalleberg - Lewis <jakth2@mail.missouri.edu>
2023-07-07 16:00:53 -0500

    move to helpers

---
+++ triotrain/helpers/jobs.py
@@ -0,0 +1,30 @@
+def is_jobid(value: Union[int, str]) -> bool:
+    """Determine if a user entered value is a SLURM job id, indicating a currently running job to save for building job dependencies.
+
+    Parameters
+    ----------
+    value : Union[int, str]
+        the value to be checked
+
+    Returns
+    -------
+    bool
+        if True, entry is a valid SLURM job ID
+    """
+    return len(str(value)) in [8] and str(value).isdigit()
+
+
+def is_job_index(value: Union[int, str], max_jobs: int = 1) -> bool:
+    """Determine if a user entered value is list index, indicating a job to be re-run.
+
+    Parameters
+    ----------
+    value : Union[int, str]
+        the value to be checked
+
+    Returns
+    -------
+    bool
+        if True, entry can be used to index a list of SLURM jobs
+    """
+    return str(value).isdigit() and 0 <= int(value) < max_jobs
--- triotrain/model_training/pipeline/run.py
+++ triotrain/model_training/pipeline/run.py
@@ -153,35 +153,7 @@ class RunTrioTrain:
                 jobids_list.insert(index, jobid)
             self.restart_jobs[key] = jobids_list
 
-    def is_jobid(self, value: Union[int, str]) -> bool:
-        """Determine if a user entered value is a SLURM job id, indicating a currently running job to save for building job dependencies.
-
-        Parameters
-        ----------
-        value : Union[int, str]
-            the value to be checked
-
-        Returns
-        -------
-        bool
-            if True, entry is a valid SLURM job ID
-        """
-        return len(str(value)) in [8] and str(value).isdigit()
-
-    def is_job_index(self, value: Union[int, str], max_jobs: int = 1) -> bool:
-        """Determine if a user entered value is list index, indicating a job to be re-run.
-
-        Parameters
-        ----------
-        value : Union[int, str]
-            the value to be checked
-
-        Returns
-        -------
-        bool
-            if True, entry can be used to index a list of SLURM jobs
-        """
-        return str(value).isdigit() and 0 <= int(value) < max_jobs
+    
 
     def process_re_runs(
         self, phase: str, total_jobs_in_phase: int = 1, genome: Union[str, None] = None
