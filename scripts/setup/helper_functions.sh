5f36a81cc87d90a80695da889ae178a4f489508c
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2024-08-05 12:08:59 -0500

    keeping old helper functions for exisiting SLURM jobs

--- scripts/setup/helper_functions.sh
+++ scripts/setup/helper_functions.sh
@@ -41,23 +41,23 @@ capture_status()
     fi
 }
 
-# # Error Handling Routine
-# error_exit()
-# {
-#     status=$?
-#     if [ $status -ne 0 ]; then
-#         # echo "Exit Status:" $status
-#         echo "ERROR: $1" >> $2
-#         exit $status
-#     fi
-# }
+# Error Handling Routine
+error_exit()
+{
+    status=$?
+    if [ $status -ne 0 ]; then
+        # echo "Exit Status:" $status
+        echo "ERROR: $1" >> $2
+        exit $status
+    fi
+}
 
-# # Success Handling Routine
-# success_exit()
-# {
-#     status=$?
-#     if [ $status -eq 0 ]; then
-#         echo "SUCCESS: $1" >> $2
-#         exit 0
-#     fi
-# }
+# Success Handling Routine
+success_exit()
+{
+    status=$?
+    if [ $status -eq 0 ]; then
+        echo "SUCCESS: $1" >> $2
+        exit 0
+    fi
+}
