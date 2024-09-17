96ab1f9f64d98d3f3122e080741dd5342645bd10
Jenna Kalleberg - Hellbender <jakth2@mail.missouri.edu>
2024-09-17 15:41:19 -0500

    removing old code

--- triotrain/summarize/calibration.py
+++ triotrain/summarize/calibration.py
@@ -89,7 +89,7 @@ def collect_args() -> argparse.Namespace:
             "--output-path",
             "/mnt/pixstor/schnabelr-drii/WORKING/jakth2/VARIANT_CALLING_OUTPUTS/230313_GIAB/summary/",
             # "--debug",
-            # "--dry-run",
+            "--dry-run",
         ]
     )
 
@@ -156,8 +156,7 @@ def __init__() -> None:
     except AssertionError as E:
         logger.error(E)
 
-    # Check for exisiting output    
-    # _processed_file = TestFile(file=f"{mie_vcf._prefix_path}.sorted.tsv", logger=logger)
+    # Check for exisiting output
     _processed_file = TestFile(file=f"{args.outpath}/{mie_vcf._prefix_name}.sorted.tsv", logger=logger)
     _processed_file.check_missing(logger_msg=logger_msg, debug_mode=args.debug)
 
@@ -245,63 +244,23 @@ def __init__() -> None:
             # Transform Mendelian Violations to boolean for efficient counting
             if row["INFO/MCV"] != ".":
                 _row_copy["IS_MIE"] = 1
-                # print("GT Values:", gt_values)
-                # breakpoint()
             else:
                 _row_copy["IS_MIE"] = 0
 
             # Save transformations to the copy made
             _variable_within_trio.insert(itr, _row_copy)
-        
-        # print("NUM UNCALLED IN OFFSPRING:", len(_uncalled_in_offspring))
-        # for i in _uncalled_in_offspring[0:5]:
-        #     print(i)
-        # breakpoint()
-        # print("NUM UNCALLED ACROSS TRIO:", len(_non_variable_within_trio))
-        # for i in _non_variable_within_trio[0:5]:
-        #     print(i)
-        # breakpoint()
-        # print("NUM OF SITES IGNORED BY RTG-TOOLS:", len(_ignored_by_rtg_tools))
-        # for i in _ignored_by_rtg_tools[0:5]:
-        #     print(i)
-        # print("NUM VARIABLE SITES:", len(_variable_within_trio))
-        # for i in _variable_within_trio[0:5]:
-        #     print(i)
-        # breakpoint()
 
         # Sort the Min. GQ from largest to smallest
         # USING THE CHILD'S GQ VALUE!
-        # _offspring = "NA24385"
         _offspring = mie_vcf._samples[0]
-        # print("OFFSPRING:", _offspring)
-        # breakpoint()
         
         sorted_dict_array = sorted(
             _variable_within_trio, key=lambda x: int(x[f"GQ_{_offspring}"]), reverse=True
-        ) 
-        # sorted_dict_array = sorted(
-        #     _variable_within_trio, key=lambda x: x["INFO/MIN_GQ"]
-        # )
-        
-        # print("NUM SORTED SITES:", len(sorted_dict_array))
-        # for i in sorted_dict_array[0:5]:
-        #     print(i)
-        # breakpoint()
-        
-        # Calculate the running MIE rate as GQ score decreases
-        # for itr,row in enumerate(sorted_dict_array):
-        #     _row_copy = row.copy()
-            
-        #     _cumulative_sum = 
-        #     print("ROW:", row)
-        #     breakpoint()
+        )
 
         logger.info(
             f"{logger_msg}: done processing contents from VCF -> TSV | '{mie_vcf._output_file.path.name}'"
         )
-        
-        # print(type(sorted_dict_array[0]))
-        # breakpoint()
 
         # Save output to disk
         results = Files(
@@ -312,81 +271,6 @@ def __init__() -> None:
         )
         results.check_status()
         results.write_list_of_dicts(line_list=sorted_dict_array, delim="\t")
-
-    # # Create bins without having to read the (very large)
-    # #   processed TSV in as a pd.DataFrame
-    # _bins = tuple(range(0, 105, 5))
-    # _counts = Bins(_bins)
-    # _counts.create_bins()
-    # # print(f"COUNTS BINS: {_counts._sdict}")
-    # # breakpoint()
-
-    # _num_mie = Bins(_bins)
-    # _num_mie.create_bins()
-    # # print(f"NUM MIE BINS: {_num_mie._sdict}")
-
-    # for itr, row in enumerate(sorted_dict_array):
-    #     _min_gq_value = int(row["INFO/MIN_GQ"])
-    #     _mie_value = int(row["IS_MIE"])
-    #     _counts[_min_gq_value] += 1
-    #     _num_mie[_min_gq_value] += _mie_value
-    #     # print(f"COUNTS BINS {itr}: {_counts._sdict}")
-    #     # print(f"NUM MIE BINS {itr}: {_num_mie._sdict}")
-    #     # breakpoint()
-
-    # # Transform the two summary dicts into a pd.DataFrame
-    # summary_data = {
-    #     "MIN_GQ_BIN": list(_counts._sdict.keys()),
-    #     "NUM_SITES": list(_counts._sdict.values()),
-    #     "NUM_MIE": list(_num_mie._sdict.values()),
-    # }
-    # # print("SUMMARY DF")
-    # # print(summary_data)
-    # # breakpoint()
-    # summary_df = pd.DataFrame(summary_data)
-    # # print("SUMMARY DF")
-    # # print(summary_df)
-    # # breakpoint()
-    # _sum_sites = summary_df["NUM_SITES"].sum()
-    # _sum_mie = summary_df["NUM_MIE"].sum()
-
-    # summary_df["%NUM_SITES"] = (summary_df["NUM_SITES"] / _sum_sites) * 100
-    # summary_df["%MIE"] = (summary_df["NUM_MIE"] / _sum_mie) * 100
-    # summary_df["%MIE_Rate"] = (summary_df["NUM_MIE"] / _sum_sites) * 100
-
-    # # Define the summary output CSV file to be created
-    # summary_file = Files(
-    #     _processed_file.path.parent / f"{_processed_file.path.stem}.calibration.csv",
-    #     logger,
-    # )
-    # summary_file.check_status()
-
-    # if args.dry_run:
-    #     logger.info(
-    #         f"{logger_msg}: pretending to write calibration CSV | '{summary_file.file_path.name}'"
-    #     )
-    #     print(summary_df)
-    # else:
-    #     if summary_file.file_exists:
-    #         logger.info(
-    #             f"{logger_msg}: found exisiting calibration CSV | '{summary_file.file_path.name}'"
-    #         )
-    #     else:
-    #         logger.info(
-    #             f"{logger_msg}: writing calibration CSV | '{summary_file.file_path.name}'"
-    #         )
-    #         summary_df.to_csv(summary_file.file_path, index=False)
-    #         if args.debug:
-    #             summary_file.check_status(should_file_exist=True)
-    #             if summary_file.file_exists:
-    #                 logger.debug(
-    #                     f"{logger_msg}: {summary_file.file_path.name} written"
-    #                 )
-
-    # logger.info(
-    #     f"{logger_msg}: total number of Mendelian Inheritance Errors (MIE): {_sum_mie:,}"
-    # )
-    # logger.info(f"{logger_msg}: total number of variants analyzed: {_sum_sites:,}")
 
     Wrapper(__file__, "end").wrap_script(timestamp())
 
